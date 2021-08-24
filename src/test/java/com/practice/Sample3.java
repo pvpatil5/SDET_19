4741057438a18bf9ca9cb03644d15d28150ed588
pvpatil5 <pvpatil123@yahoo.com>
2021-08-24 12:02:50 +0530

    New Classes Added

---
+++ src/test/java/com/practice/Sample3.java
@@ -0,0 +1,10 @@
+package com.practice;
+
+public class Sample3 {
+
+	public static void main(String[] args) {
+		System.out.println("Sample3");
+
+	}
+
+}
---
+++ src/test/java/com/practice/Sample4.java
@@ -0,0 +1,10 @@
+package com.practice;
+
+public class Sample4 {
+
+	public static void main(String[] args) {
+		System.out.println("Sample4");
+
+	}
+
+}
