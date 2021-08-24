eaa800449075390812652fa606057dce69c93c23
pvpatil5 <pvpatil123@yahoo.com>
2021-08-24 11:25:56 +0530

    New Class added

---
+++ src/test/java/com/practice/Sample1.java
@@ -0,0 +1,10 @@
+package com.practice;
+
+public class Sample1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Nee CLass Addec Sampl1");
+	}
+
+}
