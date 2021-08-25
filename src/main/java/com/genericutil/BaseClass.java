d2f50d08da02f99860a4a30fdfe41bf1fbca71fd
pvpatil5 <pvpatil123@yahoo.com>
2021-08-25 17:54:11 +0530

    New Profiles Created

--- src/main/java/com/genericutil/BaseClass.java
+++ src/main/java/com/genericutil/BaseClass.java
@@ -17,11 +17,6 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
-import com.aventstack.extentreports.ExtentReports;
-import com.aventstack.extentreports.ExtentTest;
-import com.aventstack.extentreports.Status;
-import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-import com.aventstack.extentreports.reporter.configuration.Theme;
 import com.objectrepo.HomePage;
 import com.objectrepo.LoginPage;
 
