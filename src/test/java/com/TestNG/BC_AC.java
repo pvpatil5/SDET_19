25f54011a12e10a91badf6ac2f118b56fbd04146
pvpatil5 <pvpatil123@yahoo.com>
2021-08-29 11:36:33 +0530

    Workspace 1

--- src/test/java/com/TestNG/BC_AC.java
+++ src/test/java/com/TestNG/BC_AC.java
@@ -5,10 +5,16 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class BC_AC 
 {
+	@BeforeSuite
+	public void bs() {
+		System.out.println("Connection Made");
+	}
+	
 	@BeforeClass
 	public void launch_browser() 
 	{
