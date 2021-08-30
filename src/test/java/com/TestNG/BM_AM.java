74e636bad7970bd2ef411e27a69683ec4b227133
pvpatil5 <pvpatil123@yahoo.com>
2021-08-30 10:49:28 +0530

    This Workspace 1. Tester 1

--- src/test/java/com/TestNG/BM_AM.java
+++ src/test/java/com/TestNG/BM_AM.java
@@ -2,10 +2,18 @@ package com.TestNG;
 
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class BM_AM {
+	
+	
+	@BeforeClass
+	public void launchbrowser() {
+		System.out.println("browser launched");
+	}
+	
 	
 	@BeforeMethod
 	public void logintoApp() 
