993b3939c7e9523a17c9b8e82e7299dabbc4969b
kulkarnirahul002 <kulkarnirahul002@gmail.com>
2021-09-29 17:34:25 +0530

    New Coomit Rahul sir

--- src/main/java/com/genericutil/BaseClass.java
+++ src/main/java/com/genericutil/BaseClass.java
@@ -37,6 +37,10 @@ public class BaseClass
 		System.out.println("=====JDBC Connection Done====");
 
 	}
+	
+	public void justGit() {
+		System.out.println("Git + HI");
+	}
 
 	@AfterSuite(groups = {"Smoke Test","Regression Test"})
 	public void close_JDBC_Report() {
